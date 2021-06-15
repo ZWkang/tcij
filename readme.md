184f5bde927e4aa79f0e61b8a93b37638092c152
ZWkang <kang95630@gmail.com>
2021-06-16 03:28:36 +0800

    feat: initial full commit

---
+++ readme.md
@@ -0,0 +1,15 @@
+# tiny-css-in-js
+
+> 一个很小很小的 css-in-js 实现，主要是对过往使用 css-in-js 的总结
+
+## usage
+
+```bash
+yarn add tcij
+# or
+npm install tcij
+```
+
+## License
+
+MIT
---
+++ src/parse.js
@@ -0,0 +1,70 @@
+import { compile } from 'stylis'
+
+export const rules = []
+let cache = {}
+
+let insert = rule => {
+  rules.push(rule.join(''))
+}
+
+if (typeof document !== 'undefined') {
+  const sheet = document.head.appendChild(document.createElement('style')).sheet
+  insert = rule => {
+    rules.push(rule.join(''))
+    rule.forEach(r => {
+      sheet.insertRule(r, sheet.cssRules.length)
+    })
+  }
+}
+
+// https://github.com/darkskyapp/string-hash
+function hashCode(str) {
+  var hash = 5381,
+    i = str.length
+
+  while (i) {
+    hash = (hash * 33) ^ str.charCodeAt(--i)
+  }
+
+  /* JavaScript does bitwise operations (like XOR, above) on 32-bit signed
+   * integers. Since we want the results to be always positive, convert the
+   * signed int to an unsigned by doing an unsigned bitshift. */
+  return hash >>> 0
+}
+
+export const parse = styleString => {
+  const hash = hashCode(styleString)
+  if (cache[hash]) {
+    if (rules.indexOf(cache[hash].value.join('')) > -1) {
+      return `S${hash}`
+    }
+    insert(cache[hash].value)
+    return `S${hash}`
+  }
+  const res = compile(`.S${hash}{${styleString}}`)
+  const result = []
+  function parseChildren(node) {
+    const { children, props } = node
+
+    if (children.length === 0) {
+      return
+    }
+    if (props.length > 0) {
+      props.forEach(prop => {
+        result.push(`${prop} {${children.map(o => o.value).join('')}}`)
+      })
+    }
+  }
+
+  res.forEach(parseChildren)
+  cache[hash] = {
+    value: result
+  }
+
+  insert(result)
+
+  return `S${hash}`
+}
+
+export const ruleStrings = () => rules
+export const reset = () => (rules = [])
