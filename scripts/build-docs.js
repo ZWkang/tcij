e7f5ed5cff1979e8459af74711b83715d7131235
ZWkang <kang95630@gmail.com>
2022-02-13 23:11:52 +0800

    chore: script change
    chore: script change
    
    chore: script change
    
    chore: script change
    
    chore: script change
    
    chore: script change
    
    chore: script change
    
    chore: script change
    
    chore: script change
    
    chore: script change

--- scripts/build-docs.js
+++ scripts/build-docs.js
@@ -4,15 +4,23 @@ let { version } = require('../package.json');
 
 async function backToOriginBranch() {
   await $`git checkout main`;
-
-  await $`git stash pop`;
 }
 
 void (async function () {
   await $`echo "Building docs..."`;
 
-  await $`git stash save`;
+  await $`git checkout main`;
+  await $`git pull --rebase`;
 
+  await $`rm -rf docs`;
+
+  if (await $`ls -l | grep -v "node_modules"`) {
+    await $`echo "downloading node_modules..."`;
+    await $`yarn`;
+  }
+  await $`npm run docs:build`;
+  await $`git add docs -f`;
+  await $`git stash`;
   await $`git checkout gh-pages`;
 
   try {
@@ -27,25 +35,18 @@ void (async function () {
     }
   }
 
-  await $`git merge main --squash --no-edit`;
-
   await $`rm -rf docs`;
+  await $`git stash pop`;
 
-  if (await $`ls -l | grep -v "node_modules"`) {
-    await $`echo "downloading node_modules..."`;
-    await $`yarn`;
-  }
-
-  await $`npm run docs:build`;
-
-  await $`git add docs`;
+  await $`git add docs -f`;
 
   let str = await $`git status`;
 
   if (/clean/.test(str)) {
     await $`echo "No changes to commit. return back branch"`;
   } else {
-    await $`git commit -m "docs: build ${version}"`;
+    const buildTime = await $`date +'%Y-%m-%d %H:%M:%S'`;
+    await $`git commit -m "docs: build ${version}, updated at ${buildTime}"`;
     await $`git push origin gh-pages -f`;
     await $`echo "build docs done"`;
   }
