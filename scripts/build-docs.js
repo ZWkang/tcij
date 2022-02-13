16af4d35e3ce3f51c9050a91a037ed24eb55f423
ZWkang <kang95630@gmail.com>
2022-02-13 20:13:12 +0800

    chore: in async function

--- scripts/build-docs.js
+++ scripts/build-docs.js
@@ -1,34 +1,36 @@
 #!/usr/bin/env zx
 
-let { version } = require('./package.json');
+let { version } = require('../package.json');
 
-await $`echo "Building docs..."`;
+void (async function () {
+  await $`echo "Building docs..."`;
 
-await $`git stash save`;
+  await $`git stash save`;
 
-await $`git checkout gh-pages`;
+  await $`git checkout gh-pages`;
 
-await $`git pull origin gh-pages`;
+  await $`git pull origin gh-pages`;
 
-await $`git merge main --squash --no-edit`;
+  await $`git merge main --squash --no-edit`;
 
-await $`rm -rf dist`;
+  await $`rm -rf dist`;
 
-if (await $`ls -l | grep -v "node_modules"`) {
-  await $`echo "downloading node_modules..."`;
-  await $`yarn`;
-}
+  if (await $`ls -l | grep -v "node_modules"`) {
+    await $`echo "downloading node_modules..."`;
+    await $`yarn`;
+  }
 
-await $`npm run docs:build`;
+  await $`npm run docs:build`;
 
-await $`git add -f dist`;
+  await $`git add -f dist`;
 
-await $`git commit -m "docs: build ${version}"`;
+  await $`git commit -m "docs: build ${version}"`;
 
-await $`git push origin gh-pages`;
+  await $`git push origin gh-pages`;
 
-await $`echo "build docs done"`;
+  await $`echo "build docs done"`;
 
-await $`git checkout main`;
+  await $`git checkout main`;
 
-await $`git stash pop`;
+  await $`git stash pop`;
+})();
