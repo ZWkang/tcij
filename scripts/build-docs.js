9c375048b6abd70bfe9bd773650b261caeb9a3b7
ZWkang <kang95630@gmail.com>
2022-02-13 20:01:45 +0800

    chore: script change

---
+++ scripts/build-docs.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env zx
+
+let { version } = require('./package.json');
+
+await $`echo "Building docs..."`;
+
+await $`git stash save`;
+
+await $`git checkout gh-pages`;
+
+await $`git pull origin gh-pages`;
+
+await $`git merge main --squash --no-edit`;
+
+await $`rm -rf dist`;
+
+if (await $`ls -l | grep -v "node_modules"`) {
+  await $`echo "downloading node_modules..."`;
+  await $`yarn`;
+}
+
+await $`npm run docs:build`;
+
+await $`git add dist`;
+
+await $`git commit -m "docs: build ${version}"`;
+
+await $`git push origin gh-pages`;
+
+await $`echo "build docs done"`;
+
+await $`git checkout main`;
+
+await $`git stash pop`;
